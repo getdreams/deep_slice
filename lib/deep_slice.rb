e26861965480f5b57429da43e2edb3f9dcaf522f
Didde Brockman <diddeb@users.noreply.github.com>
2016-12-02 14:18:32 +0100

    :eyes:

--- lib/deep_slice.rb
+++ lib/deep_slice.rb
@@ -13,9 +13,19 @@ class Hash
             recursive_keys = key[sub_key]
             result[sub_key] = \
               if self[sub_key].is_a?(Array)
-                self[sub_key].map { |val| val.deep_slice(*[recursive_keys].flatten) }
+                self[sub_key].map { |val|
+                  if val.nil?
+                    nil
+                  else
+                    val.deep_slice(*[recursive_keys].flatten)
+                  end
+                }
               else
-                 self[sub_key].deep_slice(*[recursive_keys].flatten)
+                 if self[sub_key].nil?
+                   nil
+                 else
+                   self[sub_key].deep_slice(*[recursive_keys].flatten)
+                 end
               end
           end
         end
